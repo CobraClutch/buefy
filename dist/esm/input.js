b4003ca2d3445f669a8318b1a4df67fe3493b239
Curtis Reinhardtsen <curtis@jw.solar>
2023-01-27 17:10:59 -0600

    Include dist

---
+++ dist/esm/input.js
@@ -0,0 +1,17 @@
+import './chunk-455cdeae.js';
+import './helpers.js';
+import './chunk-e92e3389.js';
+import './chunk-5dba4477.js';
+import './chunk-79d3e504.js';
+import { r as registerComponent, u as use } from './chunk-cca88db8.js';
+import { I as Input } from './chunk-59d8e0fb.js';
+export { I as BInput } from './chunk-59d8e0fb.js';
+
+var Plugin = {
+  install: function install(Vue) {
+    registerComponent(Vue, Input);
+  }
+};
+use(Plugin);
+
+export default Plugin;
